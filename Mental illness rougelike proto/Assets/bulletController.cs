f4caf0f5c12f5f07513b57b31fd784a7b3dd7b40
MrMurly <gustav.melefors@gmail.com>
2019-07-01 19:53:48 +0200

    bullets can now move

---
+++ Mental illness rougelike proto/Assets/bulletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour
+{
+
+	public Rigidbody2D rb;
+
+	[HideInInspector]
+	public bool flipY = false;
+
+	public float speed = 15f;
+	// Start is called before the first frame update
+    void Awake()
+    {
+		rb = transform.GetComponent<Rigidbody2D>();
+    }
+
+	void Update()
+	{
+		
+	}
+
+	private void FixedUpdate()
+	{
+		rb.MovePosition(Vector2.right * speed * Time.deltaTime + rb.position);
+	}
+}
