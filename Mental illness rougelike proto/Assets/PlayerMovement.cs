5ac8da0e294f7f234fc8c3a2658b42f54796e37f
MrMurly <gustav.melefors@gmail.com>
2019-07-01 18:45:39 +0200

    Added basic movement and sample sprites.
    move with WASD and jump with space. Basic assets are used

---
+++ Mental illness rougelike proto/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	[SerializeField]
+	private controllerMovement controller;
+
+	private float horizontalInput = 0f;
+
+	private float jumpInput = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+		horizontalInput = Input.GetAxisRaw("Horizontal");
+		jumpInput = Input.GetAxisRaw("Jump");
+	}
+
+	private void FixedUpdate()
+	{
+		controller.Move(false, jumpInput, horizontalInput);
+	}
+
+}
---
+++ Mental illness rougelike proto/Assets/controllerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controllerMovement : MonoBehaviour
+{
+	private Rigidbody2D rb;
+
+
+	//walking
+	public float moveSpeed;
+	private float horizontalInput = 0f;
+
+	//Jumping
+	public Transform feetPos; // the position of the feet to determine weather or not it is on the ground.
+	public LayerMask whatIsGround; // a layer mask to determine what is ground
+	public float jumpForce;
+
+	private void Awake()
+	{
+		rb = transform.GetComponent<Rigidbody2D>();
+	}
+
+	
+	public void Move(bool grounded, float jump, float direction)
+	{
+		//check to see weather or not on the ground.
+		grounded = Physics2D.OverlapCircle(feetPos.position, 0.3f, whatIsGround);
+
+		if (grounded && jump != 0)
+		{
+			rb.AddForce(Vector2.up * jumpForce);
+		}
+
+		//Move the player
+		rb.position += new Vector2(direction * Time.deltaTime, 0f);
+
+	
+	}
+
+}
+
+
